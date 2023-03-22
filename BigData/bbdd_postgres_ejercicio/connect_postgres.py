48c4e7ebe92ce62f5a0c1017ac763bde72fbc77c
arodgom <albertorodriguez_16@hotmail.com>
2023-03-22 22:01:12 +0100

    ejercicio postgres creado y editado

--- BigData/bbdd_postgres_ejercicio/connect_postgres.py
+++ BigData/bbdd_postgres_ejercicio/connect_postgres.py
@@ -29,26 +29,55 @@ def createDatabase():
 # createDatabase()
 
     
-def createTable(cur):
+def createTable_edicion(cur):
     try:
-        cur.execute("CREATE TABLE notas(name varchar(80), age int, grades real, date date)")
+        cur.execute("CREATE TABLE edicion(id_edic serial primary key, numero varchar)")
     except psycopg2.Error as e:
         print("Error al crear tabla: %s" % str(e))
     
-# createTable(cur) 
+# createTable_edicion(cur) 
 
-
-def insertar(cur):
+def createTable_notas(cur):
     try:
-        cur.execute("INSERT INTO notas VALUES(%s, %s, %s, %s)", ("Jose Peña", 205, 7.6, "07/07/2023"))
+        cur.execute("CREATE TABLE notas(id_notas serial primary key, name varchar, edad int, notas real, id_edic int)")
+    except psycopg2.Error as e:
+        print("Error al crear tabla: %s" % str(e))
+    
+# createTable_notas(cur) 
+
+
+def insertar_edicion(cur):
+    try:
+        cur.execute("INSERT INTO edicion VALUES(%s, %s)",
+                     (1, "Uno")
+                     (2, "Dos")
+                     (3, "Tres"))
     except psycopg2.Error as e:
         print("Error al insertar dato: %s" % str(e))
         
-# insertar(cur)
+# insertar_edicion(cur)
+
+def insertar_notas(cur):
+    try:
+        cur.execute("INSERT INTO notas VALUES(%s, %s, %s, %s, %s)",
+                    (1, "Isabel Maniega", 30, 5.6, 1),
+                    (2, "José Manuel Peña", 30, 7.8, 1),
+                    (3, "Pedro López", 25, 5.2, 2),
+                    (4, "Julia García", 22, 7.3, 1),
+                    (5, "Amparo Mayora", 28, 8.4, 3),
+                    (6, "Juan Martínez", 30, 6.8, 3),
+                    (7, "Fernando López", 35, 6.1, 2),
+                    (8, "María Castro", 41, 5.9, 3))",
+                     
+    except psycopg2.Error as e:
+        print("Error al insertar dato: %s" % str(e))
+        
+# insertar_notas(cur)
 
 def actualizar(cur):
     try:
-        cur.execute("UPDATE notas SET grades=9.5 WHERE name='Isabel Maniega'")
+        cur.execute("UPDATE notas SET notas=6.4 WHERE id_notas='3'")
+        cur.execute("UPDATE notas SET notas=5.2 WHERE id_notas='8'")
     except psycopg2.Error as e:
         print("Error al actualizar dato: %s" % str(e))
 
@@ -57,7 +86,7 @@ def actualizar(cur):
 
 def eliminar(cur):
     try:
-        cur.execute("DELETE FROM notas WHERE name='Isabel Maniega'")
+        cur.execute("DELETE FROM notas WHERE name='Pedro López'")
     except psycopg2.Error as e:
         print("Error al eliminar dato: %s" % str(e))
 
